704a8a7514c8b1f277c2770f2514d65b6ad61326
Pranaw Kumar <177040319+prana-W@users.noreply.github.com>
2026-07-25 12:20:23 +0530

    Create a multi-pdf injestion pipeline

---
+++ server/ingestion/pdf_ingestion.py
@@ -0,0 +1,47 @@
+from langchain_core.documents.base import Document
+from langchain_opendataloader_pdf import OpenDataLoaderPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+__all__ = ["process_pdfs"]
+
+def _inject_metadata(documents, extra_metadata):
+    for doc in documents:
+        doc.metadata.update(extra_metadata)
+    return documents
+
+def _load_pdf(pdf_file_names: list[str]):
+    # Create a list of full paths for the loader
+    file_paths = [f"../data/{pdf}" for pdf in pdf_file_names]
+    loader = OpenDataLoaderPDFLoader(
+        file_path=file_paths,
+        format="markdown",
+        split_pages= True,
+        image_output= "embedded"
+        
+    )
+    documents = loader.load()
+    return documents
+
+def _chunk_documents(docs):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+   
+    chunks = splitter.split_documents(docs)
+    return chunks
+
+def process_pdfs(pdf_file_names: list[str], college_slug: str):
+    """
+    Main orchestration function to process multiple PDFs natively.
+    """
+    # 1. Load all PDFs at once using the loader's native support
+    docs = _load_pdf(pdf_file_names)
+    
+    # 2. Break all documents into chunks
+    chunks = _chunk_documents(docs)
+    
+    # 3. Inject the common college_slug metadata into all chunks.
+    final_chunks = _inject_metadata(chunks, {"college_slug": college_slug})
+        
+    return final_chunks
